57e1d6ca21f9dedb6a3f8256f36d1f4cc277acc0
Carlos Eduardo Simon Zepeda <simon@industriafuturo.com>
2019-08-06 18:17:44 -0500

    ya jala! :o

--- helpers/manifest.js
+++ helpers/manifest.js
@@ -1,49 +1,54 @@
 import axios from 'axios'
 import handleError from './error'
-import safeStringify from 'fast-safe-stringify'
 import cheerio from 'cheerio'
 
 const loadManifest = name => axios.get(`${name}/manifest.json`)
 const handleManifest = (endpoint, { data: manifest }) => {
   const obj = {}
-  const main = manifest.files
-  console.log(endpoint)
-  console.log(safeStringify(manifest))
+  let main = {}
+  if (!manifest.files) {
+    main = manifest
+  } else {
+    main = manifest.files
+  }
   Object.keys(main).forEach(elem => {
     switch (elem) {
       case 'main.css':
-        obj['css'] = '/' + endpoint + main[elem]
+        obj['css'] = endpoint + main[elem]
         break
       case 'main.js':
-        obj['js'] = '/' + endpoint + main[elem]
+        obj['js'] = endpoint + main[elem]
         break
     }
   })
   return new Promise((resolve, reject) => {
     if (main['index.html']) {
       axios
-        .get('/' + endpoint + main['index.html'])
+        .get(endpoint + main['index.html'])
         .then(result => {
           const $ = cheerio.load(result.data)
           obj['html'] = $('#import-me').html()
           obj['ishtml'] = true
           resolve(obj)
         })
-        .catch(ex => reject(ex))
+        .catch(_ex => {
+          console.log('consulta 📨')
+          console.log(endpoint + main['index.html'])
+          reject(_ex)
+        })
     } else {
       obj['html'] = ''
       obj['ishtml'] = false
       resolve(obj)
     }
-    console.log(safeStringify(obj))
   })
 }
 export const get = arr => {
   const name = arr.reduce((prev, current) => `${prev}/${current}`, '')
-  console.log(name)
+  const endpoint = '/' + arr[0] + '/'
   return new Promise((resolve, reject) => {
     loadManifest(name)
-      .then(manifest => resolve(handleManifest(arr[0], manifest)))
+      .then(manifest => resolve(handleManifest(endpoint, manifest)))
       .catch(ex => {
         handleError(ex)
         reject(ex)
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ import handleError from '../helpers/error'
 
 const handle = app => {
   app.get('/app/:module/:page?/:sub?', (req, res) => {
-    const directions = Object.keys(req.params).map(k => req.params[k]).filter(Boolean)
-    console.log(directions)
+    const directions = Object.keys(req.params)
+      .map(k => req.params[k])
+      .filter(j => j && j !== 'undefined')
     get([...directions])
-      .then(data => res.render('index', data))
+      .then(data => {
+        console.log({ data })
+        res.render('index', data)
+      })
       .catch(ex => handleError(ex))
   })
 }
